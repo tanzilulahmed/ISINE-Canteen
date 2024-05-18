9718c3b515f771d510108cc50bf3d9fbfcba31c6
Tanzilul Ahmed <tanzilulahmed@Tanziluls-MacBook-Air.local>
2024-05-18 16:23:19 +0530

    footer-about-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import Navbar from "./components/Navbar/Navbar"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home'
-
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-      </Routes>
-    </div>
-  )
-}
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default App
-
+export default App;
--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -4,12 +4,16 @@ import breakfastlogo from '../assets/breakfastlogo.png'
 import drinkslogo from '../assets/drinkslogo.png'
 import maindisheslogo from '../assets/maindisheslogo.png'
 import snackslogo from '../assets/snackslogo.png'
+import foodposter from '../assets/foodposter.jpg'
+
+
 export const assets = {
     logo,
     breakfastlogo,
     drinkslogo,
     maindisheslogo,
-    snackslogo
+    snackslogo,
+    foodposter
    
 
 }
