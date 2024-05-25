3fff7abe8980cfae085c3b8899b1539e411213fd
Tanzilul Ahmed <tanzilulahmed@Tanziluls-MacBook-Air.local>
2024-05-26 00:21:36 +0530

    menu-update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
+import Menu from "./pages/Menu/Menu";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />}/>
         </Routes>
       </div>
       <Footer />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
@@ -10,8 +11,8 @@ const Navbar = () => {
     <div className= 'navbar'>
       <img src={assets.logo} alt="" className="logo" /> 
       <ul className="navbar-components">
-        <li onMouseEnter={()=>setComponents("home")} className={components=="home"?"active":""} >Home</li>
-        <li onMouseEnter={()=>setComponents("menu") }className={components=="menu"?"active":""} >Menu</li>
+        <li onMouseEnter={()=>setComponents("home")} className={components=="home"?"active":""} > <Link to="/"> Home</Link></li>
+        <li onMouseEnter={()=>setComponents("menu") }className={components=="menu"?"active":""} ><Link to="/menu"> Menu</Link></li>
         <li onMouseEnter={()=>setComponents("about")} className={components=="about"?"active":""} >About</li>
         <li onMouseEnter={()=>setComponents("contact")} className={components=="contact"?"active":""} >Contact</li>
         <li onMouseEnter={()=>setComponents("feedback") }className={components=="feedback"?"active":""} >Feedback</li>
